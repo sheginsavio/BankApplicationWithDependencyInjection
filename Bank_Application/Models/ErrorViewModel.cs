080005ea735a7d01a43515c45a93b41834e20d07
sheginsavio <savioshegin@gmail.com>
2026-03-30 22:19:36 +0530

    Remove all files from repository tracking

--- Bank_Application/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Bank_Application.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
